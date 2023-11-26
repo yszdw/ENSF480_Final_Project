38777c1305512027217c9464db7ec70a16770b9a
Aryan Karadia <karadiaaryan@gmail.com>
2023-11-25 21:34:16 -0700

    Basic Constructor fixes

--- src/CrewMember.java
+++ src/CrewMember.java
@@ -1,31 +1,21 @@
 package src;
 
 public class CrewMember extends User{
-    String crewMemberID;
     String crewMemberPos;
 
-    public CrewMember(String username, String email, String address, String creditCard, String crewMemberID,
-                      String crewMemberPos) {
-        //super(username, email, address, creditCard, "crew");
-        this.crewMemberID = crewMemberID;
+    public CrewMember(int crewMemberID, String username, String email, String address, String crewMemberPos) {
+        super(crewMemberID, username, email, address);
         this.crewMemberPos = crewMemberPos;
     }
 
     public CrewMember() {
         super();
-        this.crewMemberID = "";
         this.crewMemberPos = "";
     }
 
     /* Getters and Setters */
-    public void setCrewMemberID(String crewMemberID) {
-        this.crewMemberID = crewMemberID;
-    }
     public void setCrewMemberPos(String crewMemberPos) {
         this.crewMemberPos = crewMemberPos;
-    }
-    public String getCrewMemberID() {
-        return crewMemberID;
     }
     public String getCrewMemberPos() {
         return crewMemberPos;
