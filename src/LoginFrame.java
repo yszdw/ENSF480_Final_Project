2939c1a5e16b576b925ac2b41387a07cc95f7fb6
aaron-dalbroi <aaron.dalbroi@ucalgary.ca>
2023-11-27 15:29:41 -0700

    semicolon

--- src/LoginFrame.java
+++ src/LoginFrame.java
@@ -9,8 +9,8 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.time.LocalTime
-import java.time.LocalDate
+import java.time.LocalTime;
+import java.time.LocalDate;
 import java.util.ArrayList;
 
 public class LoginFrame extends JFrame {
